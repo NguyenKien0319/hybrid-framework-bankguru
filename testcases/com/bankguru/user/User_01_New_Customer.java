b448e648250426246314d3fbb81f4873fde80962
Nguyen Kien <nguyenkien0319@gmail.com>
2021-09-07 21:15:44 +0700

    Init new automation framework

---
+++ testcases/com/bankguru/user/User_01_New_Customer.java
@@ -0,0 +1,22 @@
+package com.bankguru.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class User_01_New_Customer {
+  @Test
+  public void New_Customer_01_Validate_Name() {
+  }
+  @Test
+  public void New_Customer_02_Validate_Phone() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
