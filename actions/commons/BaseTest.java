e121a87744b544e82a11e43289081de12f70e4d2
Nguyen Kien <nguyenkien0319@gmail.com>
2021-09-14 20:48:24 +0700

    Add Level_04_Register_Login_Page_Object nopCommerce

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,32 @@
 package commons;
 
-public class BaseTest {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+	private WebDriver driver;
+	private String projectPath = System.getProperty("user.dir");
+
+	public WebDriver getBrowserDriver(String browserName, String appUrl) {
+		if (browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			driver = new EdgeDriver();
+		} else {
+			throw new RuntimeException("Please enter correct browser name");
+		}
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(appUrl);
+		return driver;
+	}
 }
---
+++ testcases/com/nopcommerce/login/Level_04_Register_Login_Page_Object.java
@@ -0,0 +1,80 @@
+package com.nopcommerce.login;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+import pageObjects.nopCommerce.HomePageObject;
+import pageObjects.nopCommerce.LoginPageObject;
+import pageObjects.nopCommerce.RegisterPageObject;
+
+public class Level_04_Register_Login_Page_Object extends BaseTest{
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String emailAddress, password;
+	
+	@Parameters ({"browser", "url"})
+	@BeforeClass
+	public void BeforeClass(String browserName, String url) {
+		
+		driver  = getBrowserDriver(browserName, url);
+		
+		emailAddress = getEmailRandom();
+		password = "123123";
+	}
+	
+	@Test
+	public void Login_01_Register_To_System() {
+		homePage = new HomePageObject(driver);
+		Assert.assertTrue(homePage.isSliderDisplayed());
+		homePage.clickToRegisterLink();
+		registerPage  = new RegisterPageObject(driver);
+		Assert.assertTrue(registerPage.isTextRegisterDisplayed());
+		registerPage.clickToGenderMaleRadioButton();
+		registerPage.inputFirstNameTextBox("Automation");
+		registerPage.inputLastNameTextBox("Testing");
+		registerPage.inputEmailTextBox(emailAddress);
+		registerPage.inputPasswordTextBox(password);
+		registerPage.inputConfirmPasswordTextBox(password);
+		registerPage.clickRegisterButton();
+		
+		Assert.assertTrue(registerPage.isSuccesedMessageDisplayed());
+		registerPage.clickToLogoutButton();		
+		
+		homePage = new HomePageObject(driver);
+		Assert.assertTrue(homePage.isSliderDisplayed());
+	}
+	
+	@Test
+	public void Login_02_Login_To_System() {
+		homePage.clickToLoginButton();
+		loginPage = new LoginPageObject(driver);
+		loginPage.inputEmailTextBox(emailAddress);
+		loginPage.inputPasswordTextBox(password);
+		loginPage.clickLoginButton();
+		
+		homePage = new HomePageObject(driver);
+		Assert.assertTrue(homePage.isSliderDisplayed());
+	}
+	
+	@AfterClass
+	public void AfterClass() {
+		driver.close();
+	}
+	
+	public String getEmailRandom() {
+		Random rand = new Random();
+		return "testing" + rand.nextInt(999999) + "@hotmail.net";
+	}
+	HomePageObject homePage;
+	RegisterPageObject registerPage;
+	LoginPageObject loginPage;
+	
+}
